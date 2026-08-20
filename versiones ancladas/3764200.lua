b371891cce1ca16f0d91789e6f3618ab0186f114
Sheaker <111479142+RagnarokManifests@users.noreply.github.com>
2026-08-20 16:48:16 -0400

    Add files via upload

---
+++ versiones ancladas/3764200.lua
@@ -0,0 +1,44 @@
+-- 3764200 - Resident Evil Requiem
+-- Version-locked to Build 22227314 (Version 2.1.2 · Realms of Supremacy update for 6 March 2026) — released 2026-03-06 09:24:09 UTC
+-- Generated 2026-08-20 20:46:59 UTC
+-- # Depots (Total/DLC/Shared): 13/8/1
+
+-- Main AppID
+addappid(3764200, 1, "d29b7ad461b8b74dce38b7137fc00d4b5a885c5be1211767a4ecae0eaf8ddd96")
+
+-- Main Depots
+addappid(3764201, 1, "bdf443ffde6449192b9863c0fa5e3cda31fdaa5e4d33e5bcded62dc2085b7cb8")
+setManifestid(3764201, "6015379827053895104")
+addappid(3764202, 1, "3b6b7669a064598e8f48e38776d1862941f78d515a88b89333c2dbde0a07fcff")
+setManifestid(3764202, "3454638618371402255")
+addappid(3764203, 1, "b2e820287e6e3f32d1ccb3f2f8c95ec3783e8c0a75abab5d311a0a648626fd92")
+setManifestid(3764203, "5563925408499865260")
+addappid(3764204, 1, "5c0b40c6708cb9d8c0d0d780717a925508eb763c3a0c9bd74f6e76132785bc97")
+setManifestid(3764204, "5061660482225781286")
+
+-- DLC's (with depot keys)
+-- Resident Evil Requiem - Grace's Costume: Apocalypse (AppID: 3990800)
+addappid(3990800)
+addappid(3764205, 1, "25a058f4301da5d231a8e5ccdd648c2fbc5aa686cd4669012901b5f90e4ff1cb")
+setManifestid(3764205, "4436866528626191669")
+addappid(3990800, 1, "f5bf116706491176c9eee46a96fe1faa70b3f771d75ccb34f0c8962097a907e5")
+setManifestid(3990800, "9181787779883827112")
+-- Resident Evil Requiem - Deluxe Kit (AppID: 3990820)
+addappid(3990820)
+addappid(3764206, 1, "960c01ef80e38e1993ada744b48c5dd59eabf9de2726f222d579ad5c4160b10e")
+setManifestid(3764206, "136565968785594279")
+addappid(3990820, 1, "9ae4713cb4114effdd34d1cd54dbda8a560f27a05aef06fbaa0370748f69d15c")
+setManifestid(3990820, "509708311968316285")
+-- Resident Evil Requiem Original Soundtrack (AppID: 4460240)
+addappid(4460240)
+addappid(4460241, 1, "8a08c4f0a4f659b5068003bcdf72daede745db9a881d5d4021102273fdecfe59") -- Japanese
+setManifestid(4460241, "4190523507411811525")
+addappid(4460243, 1, "3d9be12e9ef64f2554c99b8ec4aaf62efa30c1f254720f4d32f760e6b4af8d8b")
+addappid(4460244, 1, "09854b8a7b7f643c28f451c16daa123a5eeb74091fbabb1c6b4cfd8550b798ff")
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4460242
+
+-- Too new to lock (no manifest at this build): 4460243, 4460244
