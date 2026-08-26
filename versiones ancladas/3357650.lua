26702f3fc3249b424f9d5faefb2821e2f32e5766
Sheaker <111479142+RagnarokManifests@users.noreply.github.com>
2026-08-26 18:46:48 -0400

    Add files via upload

---
+++ versiones ancladas/3357650.lua
@@ -0,0 +1,30 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3357650 - PRAGMATA
+-- Version-locked to Build 22357085 — released 2026-03-24 05:49:31 UTC
+-- Generated 2026-08-07 06:06:41 UTC
+-- # Depots (Total/DLC/Shared): 6/3/1
+
+-- Main AppID
+addappid(3357650, 1, "7d3fd8efe5fba21ded0c274905926cc56b6a26da1060d76462721945a8ef328a")
+
+-- Main Depots
+addappid(3357651, 1, "18e75bfb3f8c26b7a59d9e3a7013e74e1c59fe563aa00ee15f54a35b17691766")
+setManifestid(3357651, "2417499809052404547")
+addappid(3357652, 1, "7421326d63bf53701559733582fe45877e218f41683ebc6d614905727f5ba144")
+
+-- DLC's (with depot keys)
+-- PRAGMATA: Outfit Set - Neo Bushido & Neo Kunoichi (AppID: 3859920)
+addappid(3859920)
+addappid(3859920, 1, "1fb811843160e2a11211f5bba90ece0ef2dac5994c19936e50ff85faf2f80423")
+setManifestid(3859920, "4731286747379700304")
+-- PRAGMATA: Shelter Variety Pack (AppID: 3859930)
+addappid(3859930)
+addappid(3859930, 1, "14001799a6877a14751af43adbd59ca3cc037cf202aa5615e0c70e057090ddce")
+setManifestid(3859930, "6714427611547107917")
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4093670
+
+-- Too new to lock (no manifest at this build): 3357652
